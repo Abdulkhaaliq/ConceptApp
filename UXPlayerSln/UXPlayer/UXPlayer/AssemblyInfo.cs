ab253112795d617bd5e91b21d1e16747b6961fac
Abdulkhaaliq <55828788+Abdulkhaaliq@users.noreply.github.com>
2019-11-11 09:23:08 +0200

    added
    whatsapp layout

---
+++ UXPlayerSln/UXPlayer/UXPlayer/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using Android.App;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
---
+++ UXPlayerSln/UXPlayer/UXPlayer/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UXPlayer
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var Entry = new ToolbarItem
+           {  
+              IconImageSource = "search.png" 
+
+            };  
+             var Button = new ToolbarItem  
+             {  
+                 Text = "New broadcast",
+
+                 Order = ToolbarItemOrder.Secondary  
+
+             };  
+
+             this.ToolbarItems.Add(Entry);  
+             this.ToolbarItems.Add(Button); 
+        }
+
+    }
+}
